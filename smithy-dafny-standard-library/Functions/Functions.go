190c1a53e242af402b23b4624054b4799a3480a6
Shubham Chaturvedi <scchatur@amazon.com>
2025-01-07 15:46:00 -0800

    release: v0.0.1-alpha

---
+++ smithy-dafny-standard-library/Functions/Functions.go
@@ -0,0 +1,31 @@
+// Package Functions
+// Dafny module Functions compiled into Go
+
+package Functions
+
+import (
+	os "os"
+
+	m_BoundedInts "github.com/ShubhamChaturvedi7/aws-cryptographic-material-providers-library-go/smithy-dafny-standard-library/BoundedInts"
+	m__Math "github.com/ShubhamChaturvedi7/aws-cryptographic-material-providers-library-go/smithy-dafny-standard-library/Math_"
+	m_Relations "github.com/ShubhamChaturvedi7/aws-cryptographic-material-providers-library-go/smithy-dafny-standard-library/Relations"
+	m_Seq "github.com/ShubhamChaturvedi7/aws-cryptographic-material-providers-library-go/smithy-dafny-standard-library/Seq"
+	m_Seq_MergeSort "github.com/ShubhamChaturvedi7/aws-cryptographic-material-providers-library-go/smithy-dafny-standard-library/Seq_MergeSort"
+	m__Unicode "github.com/ShubhamChaturvedi7/aws-cryptographic-material-providers-library-go/smithy-dafny-standard-library/Unicode_"
+	m_Wrappers "github.com/ShubhamChaturvedi7/aws-cryptographic-material-providers-library-go/smithy-dafny-standard-library/Wrappers"
+	m__System "github.com/dafny-lang/DafnyRuntimeGo/v4/System_"
+	_dafny "github.com/dafny-lang/DafnyRuntimeGo/v4/dafny"
+)
+
+var _ = os.Args
+var _ _dafny.Dummy__
+var _ m__System.Dummy__
+var _ m_Wrappers.Dummy__
+var _ m_Relations.Dummy__
+var _ m_Seq_MergeSort.Dummy__
+var _ m__Math.Dummy__
+var _ m_Seq.Dummy__
+var _ m_BoundedInts.Dummy__
+var _ m__Unicode.Dummy__
+
+type Dummy__ struct{}
